b4f123d5f6775211e88b839d8a8f18da14711a3d
Can Kologlu <mckologlu@gmail.com>
2023-05-21 19:34:47 -0400

    add destroy component

--- horses/src/main.jsx
+++ horses/src/main.jsx
@@ -6,6 +6,7 @@ import Root, { loader as rootLoader, action as rootAction } from './routes/root.
 import ErrorPage from './components/error-page.jsx'
 import Contact, { loader as contactLoader } from './routes/contact.jsx'
 import EditContact, { action as editAction } from './routes/edit'
+import {action as destroyAction}from './routes/destroy'
 
 
 const router = createBrowserRouter([
@@ -26,6 +27,11 @@ const router = createBrowserRouter([
         element: <EditContact />,
         loader: contactLoader,
         action: editAction,
+      },
+      {
+        path:"contacts/:contactId/destroy",
+        loader: contactLoader,
+        action: destroyAction,
       }
     ]
   },
