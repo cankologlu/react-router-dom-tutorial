30fc43236d812b1d744b076319ee5fc169379c37
Can Kologlu <mckologlu@gmail.com>
2023-05-22 20:55:52 -0400

    synchronize input value with search params

--- horses/src/routes/root.jsx
+++ horses/src/routes/root.jsx
@@ -1,9 +1,15 @@
 import { Outlet, useLoaderData, Form, redirect, NavLink, useNavigation } from "react-router-dom";
 import { getContacts, createContact } from "../contacts";
+import { useEffect } from "react";
 
 export default function Root() {
-  const { contacts } = useLoaderData();
+  const { contacts, q } = useLoaderData();
   const navigation = useNavigation();
+
+useEffect(() => {
+  document.getElementById("q").value = q;
+}, [q]);
+
   return(
   <>
     <div id="sidebar">
@@ -16,6 +22,7 @@ export default function Root() {
             placeholder="Search"
             type="search"
             name="q"
+            defaultValue={q}
           />
           <div id="search-spinner" aria-hidden hidden={true} />
           <div className="sr-only" aria-live="polite"></div>
@@ -59,7 +66,7 @@ export async function loader ({request}){
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return {contacts};
+  return {contacts, q};
 }
 
 export async function action() {
